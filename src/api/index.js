2379502d5870e74badb354fa3ba21a8bccbd677a
Radoslaw Potocki <potockra@oit.rutgers.edu>
2016-11-03 16:07:31 -0400

    minor code cleanup

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ const Api = {
   getProducts: (cb) => {
     return m.request({
       method: 'GET',
-      url: '/products.json'
+      url: './products.json'
     }).run(cb);
   },
   buyProducts: (payload, cb, timeout=500) => {
